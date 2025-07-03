16228cbb69a24193d68b3a177c7b9ea659e30334
Guilherme <guilhermegpereira26@hotmail.com>
2025-07-03 06:12:25 -0300

    Starting project

---
+++ src/main/java/io/github/Guimaraes131/watchlistapi/WatchListApiApplication.java
@@ -0,0 +1,13 @@
+package io.github.Guimaraes131.watchlistapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WatchListApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WatchListApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/Guimaraes131/watchlistapi/WatchListApiApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.Guimaraes131.watchlistapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WatchListApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
